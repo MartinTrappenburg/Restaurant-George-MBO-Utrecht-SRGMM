b94e867888b23511e1a26af275630b2947f85b29
LAPTOP-0TH2B7ID\martin <martintrap1@gmail.com>
2021-09-27 14:53:53 +0200

    commit
    type: commit
    changes: adjusted reserveren page with a template. Styling needs to be changed and Javascript is being added.

--- navbar.php
+++ navbar.php
@@ -2,8 +2,8 @@
 $active = (isset($_GET["content"])) ? $_GET["content"] : "";
 ?>
 <nav class="navbar navbar-expand-md navbar-light bg-light">
-  <a class="navbar-brand" href="./index.php?content=home">Restaurant George</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <a class="navbar-brand ms-auto" href="./index.php?content=home">Restaurant George</a>
+  <button class="navbar-toggler ms-auto" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
