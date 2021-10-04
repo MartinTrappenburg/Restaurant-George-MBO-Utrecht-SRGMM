0ea7c821edb06f8ffebc3ccf4d639384d7b3aef6
callofdeath <60213819+callofdeath@users.noreply.github.com>
2021-10-04 14:20:59 +0200

    Add files via upload

---
+++ coronabeleid.php
@@ -0,0 +1,14 @@
+<h1>Met ingang vanaf 25 september 2021 gaan de nieuwste maatregelen van start.</h1>
+
+<h7>Met elke persconferentie zijn er weer nieuwe veranderen en ook ditmaal zijn er weer veranderingen. </h7>
+<p>Met ingang vanaf 25 september 2021 moeten wij weer de nieuwe maatregelen gaan handhaven. </p> 
+<p>Deze maatregelen zijn als volgt:</p>
+<p>Vanaf heden zijn wij verplicht om van iedere klant een <b>GELDIGE</b> QR-code te scannen. Dit is <b>VERPLICHT</b> voor iedereen in die aanwezig is</p>
+<p>Gelukkig mogen wij door dit te doen, wel de 1,5m afschaffen!</p>
+<p>Dit zorgt er natuurlijk ook voor dat wij weer meer mensen mogen toelaten in ons pand!</p>
+
+<!-- <h7>Dit houdt in dat IEDEREEN een geldige QR-code en legitimatie bij de hand moet hebben.
+
+Gelukkig mogen wij wel de 1,5m regel en het éénrichtingsverkeer afschaffen.</h7> -->
+
+<p> <img src="./img/george.jpg" width="256" height="256"> </p>
