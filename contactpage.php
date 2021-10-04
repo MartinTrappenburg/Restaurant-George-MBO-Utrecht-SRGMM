20b631b7863ccc8fbae78bea6ea9b0c6c9f5ef4b
Sherick16 <71008288+Sherick16@users.noreply.github.com>
2021-10-04 13:47:18 +0200

    Added contactpage.php
    -Added contact page
    -Added address and phone number
    -Added google maps

---
+++ contactpage.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="map-css" style="text-align:center">
+        <h1>Contact</h1>
+        <ul class="list-group border">
+            <h7>Spaklerweg 10A</h7>
+            <h7>1096 BA Amsterdam</h7>
+            <h7>+ 31 20 737 0280</h7>
+            <h7>hello@georgemarina.nl</h7>
+            <h7>OPEN DAILY FROM 11A.M. TILL LATE</h7>
+            <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1408.1258115520636!2d4.917592620986136!3d52.34196864436473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c60bd51c216ec7%3A0x2b8b8d2f1f99803!2sSpaklerweg%2010a%2C%201096%20BA%20Amsterdam!5e0!3m2!1sen!2snl!4v1632145822756!5m2!1sen!2snl" width="250" height="114" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+        </ul>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
