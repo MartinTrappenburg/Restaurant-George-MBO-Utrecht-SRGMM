40c948717a91480998eb7a69ab9aa728772cd0d9
Sherick16 <ajaxsdvbc3to@Gmail.com>
2021-10-25 22:22:18 +0200

    Added style for about-us & coronabeleid
    Added style for about-us & coronabeleid

--- about-us.php
+++ about-us.php
@@ -4,29 +4,31 @@
 <link rel="stylesheet" href="style.css">
 
 <head>
-<h1> Wij zijn restaurantGeorge!</h1>
 </head>
 
 <body>
-<div class="vl">
+    <div class="container center" id="aboutuscontainer">
+        <div id="aboutus1">
+            <h3> Wij zijn restaurant George!</h3>
+            <h4>Wij zijn een groep van unique restauranten gelokaliseert in amsterdam met een vleugje George DNA</h4>
+            <p1>Wij geloven in dat stukje 'je-ne-sais-quoi' op het menu, evenals fabuleus eten, elegante wijnen en geweldige service met een mooie glimlach!</p1>
+        </div>
+        <div id="aboutus2">
+            <h4>GEORGE HEEFT HET ALLEMAAL</h4>
+            <p1>Van die lokale buurtplek waar iedereen je naam kent tot een licht, eclectisch restaurant met uitzicht op de Amstel: George heeft het allemaal.<br>
+            We serveren iconische gerechten en tijdloze klassiekers met New York, Parijs, de Côte d'Azur en Hong Kong in gedachten, samen met lokale ingrediënten,vers gebrande koffie en kenmerkende cocktails. <br>
+            Onze restaurants zijn de hele dag open, elke dag - dus kom bij ons voor een espresso in de vroege ochtend, een luie zondagse brunch of een intiem diner met vrienden in een ontspannen, elegante sfeer.<br>
+        </p1> 
+        </div>
+        <div id="aboutus3">
+            <h4>EEN FEEST VOOR JE ZINTUIGEN</h4>
+            <p1>De meeste van onze restaurants zijn ontworpen door de Amsterdamse Architecture & Design studio Framework,<br>
+            die tekende voor onze op maat gemaakte interieurs en verfijnde maar intieme ambiance, met typische George-elementen zoals metrotegels, weelderige leren banken, koperen verlichting en marmeren bars. <br>
+            Omdat elke locatie zijn eigen charme heeft, is uw bezoek aan George letterlijk een lust voor het oog en uw smaakpapillen.
+            De meeste van onze locaties hebben prachtige, zonnige terrassen en zijn de hele dag open, elke dag voor ontbijt, lunch en diner.
+        </p1>
+        </div>
 
-<h2>Wij zijn een groep van unique restauranten gelokaliseert in amsterdam met een vleugje George DNA</h2>
-<p>Wij geloven in dat stukje 'je-ne-sais-quoi' op het menu, evenals fabuleus eten, elegante wijnen en geweldige service met een mooie glimlach!</p>
-<hr>
-<h5>GEORGE HEEFT HET ALLEMAAL</h5>
-<p>Van die lokale buurtplek waar iedereen je naam kent tot een licht, eclectisch restaurant met uitzicht op de Amstel: George heeft het allemaal.<br>
-
-We serveren iconische gerechten en tijdloze klassiekers met New York, Parijs, de Côte d'Azur en Hong Kong in gedachten, samen met lokale ingrediënten,vers gebrande koffie en kenmerkende cocktails. <br>
-Onze restaurants zijn de hele dag open, elke dag - dus kom bij ons voor een espresso in de vroege ochtend, een luie zondagse brunch of een intiem diner met vrienden in een ontspannen, elegante sfeer.<br>
-<hr>
-<h5>EEN FEEST VOOR JE ZINTUIGEN</h5>
-De meeste van onze restaurants zijn ontworpen door de Amsterdamse Architecture & Design studio Framework,<br> 
-die tekende voor onze op maat gemaakte interieurs en verfijnde maar intieme ambiance, met typische George-elementen zoals metrotegels, weelderige leren banken, koperen verlichting en marmeren bars. <br>
-Omdat elke locatie zijn eigen charme heeft, is uw bezoek aan George letterlijk een lust voor het oog en uw smaakpapillen.
-De meeste van onze locaties hebben prachtige, zonnige terrassen en zijn de hele dag open, elke dag voor ontbijt, lunch en diner.
-
-</div>
-
-
+    </div>
 </body>
 </html>
