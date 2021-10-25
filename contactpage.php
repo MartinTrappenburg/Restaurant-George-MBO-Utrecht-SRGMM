b7b8959925b0d960a6178cc92fe3834153a331f5
Sherick16 <71008288+Sherick16@users.noreply.github.com>
2021-10-25 14:10:03 +0200

    Added George Mariana Style
    - Added the George Marina style, layout and map.

--- contactpage.php
+++ contactpage.php
@@ -9,20 +9,32 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
 
-    <title>Hello, world!</title>
+    <title>Contact</title>
   </head>
   <body>
-    <div class="map-css" style="text-align:center">
-        <h1>Contact</h1>
-        <ul class="list-group border">
-            <h7>Spaklerweg 10A</h7>
-            <h7>1096 BA Amsterdam</h7>
-            <h7>+ 31 20 737 0280</h7>
-            <h7>hello@georgemarina.nl</h7>
-            <h7>OPEN DAILY FROM 11A.M. TILL LATE</h7>
-            <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1408.1258115520636!2d4.917592620986136!3d52.34196864436473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c60bd51c216ec7%3A0x2b8b8d2f1f99803!2sSpaklerweg%2010a%2C%201096%20BA%20Amsterdam!5e0!3m2!1sen!2snl!4v1632145822756!5m2!1sen!2snl" width="250" height="114" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
-        </ul>
-    </div>
+    <div class="center">
+      <div id="container">
+        <div id="contact-first">
+          <ul class="list-group center">
+            <h3>CONTACT</h3>
+            <p1>Spaklerweg 10A</p1>
+            <p1>1096 BA Amsterdam</p1>
+            <p1>+ 31 20 737 0280</p1>
+            <p1>hello@georgemarina.nl</p1>
+            <p1>OPEN DAILY FROM 11A.M. TILL LATE</p1>
+          </ul>
+        </div>
+        <div id="contact-map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.5681755067185!2d4.91655361580083!3d52.3419795797809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c60bd4c75894bd%3A0xcdcb9a745b29464d!2sGeorge%20Marina!5e0!3m2!1sen!2snl!4v1633958604643!5m2!1sen!2snl" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <div id="contact-second">
+          <ul class="list-group center">
+            <h3>OPENING HOURS</h3>
+            <p1>We'll rock your boat at George Marina</p1>
+            <p1>On sunny days we have valet parking for a fee of €12,50 during lunch and €15 during dinner</p1>
+          </ul>
+        </div>
+      </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
