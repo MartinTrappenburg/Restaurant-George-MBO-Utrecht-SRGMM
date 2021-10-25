a9c243e5dbf0c75f56c071258fb9a284d06a30c0
Sherick16 <71008288+Sherick16@users.noreply.github.com>
2021-10-25 15:21:37 +0200

    Initial commit
    - added book event page
    - added information
    - added image

---
+++ bookeventpage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book event</title>
+</head>
+<body>
+    <div>
+        <div>
+            <ul class="list-group center">
+               <p>Example</p> 
+            </ul>
+        </div>
+        <div>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
