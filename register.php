cc3e3cbd6f59f2b8636d2e4272440b061860c534
MartinTrappenburg <martintrap1@gmail.com>
2021-10-25 20:27:44 +0200

    update register.
    finished the basic content and styling for the register page.

--- register.php
+++ register.php
@@ -9,15 +9,31 @@
 </head>
 
 <body>
-    <form action="./index.php?content=register_script" method="post">
-        <div class="form-group">
-            <label for="inputEmail">Vul hier uw e-mailadres in:</label>
-            <input name="email" type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" autofocus>
-            <small id="emailHelp" class="form-text text-muted">Uw privacy is gegarandeerd...</small>
-        </div>
+    <div id="register_page">
+        <form action="./index.php?content=register_script" method="post">
+            <div class="form-group">
+                <label for="inputEmail">Vul hier uw e-mailadres in:</label>
+                <input name="email" type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" autofocus>
 
-        <button type="submit" class="btn btn-warning btn-lg btn-block">Registreer</button>
-    </form>
+                <label for="inputfname">Voornaam</label>
+                <input name="fname" type="text" class="form-control" id="inputfname" aria-describedby="fnameHelp" autofocus>
+
+                <label for="inputinfix">Tussenvoegsel</label>
+                <input name="infix" type="text" class="form-control" id="inputinfix" aria-describedby="infixHelp" autofocus>
+
+                <label for="inputlname">Achternaam</label>
+                <input name="lname" type="text" class="form-control" id="inputlname" aria-describedby="lnameHelp" autofocus>
+
+                <label for="inputphonenumber">Telefoonnummer</label>
+                <input name="phonenumber" type="tel" class="form-control" id="inputphonenumber" aria-describedby="phonenumberHelp" autofocus>
+                <br>
+                <input type="checkbox" name="privacy" id="privacy">
+                <label for="privacy">Ik accepteer de privacy voorwaarden.</label>
+            </div>
+
+            <button type="submit" class="btn btn-warning btn-lg btn-block">Registreer</button>
+        </form>
+    </div>
 </body>
 
 </html>
