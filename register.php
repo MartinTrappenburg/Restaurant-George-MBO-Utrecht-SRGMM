e4ab0e00fcf413c8096bdcda916ecc3d8b713688
MartinTrappenburg <martintrap1@gmail.com>
2021-10-25 20:49:23 +0200

    quick patch

--- register.php
+++ register.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-
 <body>
     <div id="register_page">
         <form action="./index.php?content=register_script" method="post">
@@ -35,5 +25,3 @@
         </form>
     </div>
 </body>
-
-</html>
