35bad798e3b18661fcf034733fcabf3721373a64
Sherick16 <ajaxsdvbc3to@Gmail.com>
2021-10-25 22:53:50 +0200

    Updated carreerpage, navbar & reserveren
    Updated carreerpage, navbar & reserveren

--- carreerpage.php
+++ carreerpage.php
@@ -8,30 +8,32 @@
 <!-- <i> = schuingedrukt <u> = onderstreept <b> = vetgedrukt  -->
 
 <body>
-	<p align="center">
-		<h4>Wil jij een leuk zakcentje verdienen als horecapersoneel? dan zit jij bij ons goed!</h4></p>
-	<p> Wij zijn altijd opzoek naar het leukste personeel!
+	<div class="center">
+		<h3>Wil jij een leuk zakcentje verdienen als horecapersoneel? dan zit jij bij ons goed!</h3>
+		<p1> Wij zijn altijd opzoek naar het leukste personeel!</p1>
 		<br>
 		<!-- Open werkfuncties -->Momenteel zijn dit onze open sollicitaties voor het horecapersoneel:
 		<br> <span style="text-decoration: line-through;">
-<i><u>kok, keukenmedewerker, afwasser</i></u>
-</span> , bartender en bediening
+	<i><u>kok, keukenmedewerker, afwasser</i></u>
+	</span> , bartender en bediening
 		<!-- Verwachtingen van de sollicitant -->
-		<p> </p>
-		<h5> wat wij van jou verwachten:</h5>
-		<p>1. Positieve werkhouding en goede mood!
+		<p1> </p1>
+		<h4> WAT WIJ VAN JOU VERWACHTEN:</h4>
+		<p1>1. Positieve werkhouding en goede mood!
 			<br> 2. Verantwoordelijkheid.
-			<br> 3. Weekend uren willen en kunnen draaien.</p>
+			<br> 3. Weekend uren willen en kunnen draaien.</p1>
 		<!-- Wat wij onze werknemers aanbieden -->
-		<h5>wat wij bieden:</h5>
-		<p>1. Een leuke en gezellige sfeer met collega's!
+		<h4>WAT WIJ BIEDEN:</h4>
+		<p1>1. Een leuke en gezellige sfeer met collega's!
 			<br> 2. Flexibele werk uren!
 			<br> 3. Een goed salaris!
 			<br> 4. Doorgroei mogelijkheden.
-			<br> 5. Schone en veilige werkomstandigheden!</p>
+			<br> 5. Schone en veilige werkomstandigheden!</p1>
 		<!-- afsluiting -->
-		<p>Denk jij te hebben wat wij zoeken? Meld je dan snel aan en wij nodigen jou snel uit voor een gesprek
-			<br> Mail jouw CV naar : restaurantgeorge@gmail.com of kom even langs bij ons in het restaurant!</p>
+		<p1>Denk jij te hebben wat wij zoeken? Meld je dan snel aan en wij nodigen jou snel uit voor een gesprek
+			<br> Mail jouw CV naar :
+			<p2>restaurantgeorge@gmail.com</p2> of kom even langs bij ons in het restaurant!</p1>
+	</div>
 </body>
 
 </html>
