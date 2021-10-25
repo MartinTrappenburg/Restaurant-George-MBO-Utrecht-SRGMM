18d5526159217176782be2d726136fbe3d755bcd
callofdeath <60213819+callofdeath@users.noreply.github.com>
2021-10-25 15:33:30 +0200

    add: about-us
    add: about-us

---
+++ about-us.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" href="style.css">
+
+<head>
+<h1> Wij zijn restaurantGeorge!</h1>
+</head>
+
+<body>
+<div class="vl">
+
+<h2>Wij zijn een groep van unique restauranten gelokaliseert in amsterdam met een vleugje George DNA</h2>
+<p>Wij geloven in dat stukje 'je-ne-sais-quoi' op het menu, evenals fabuleus eten, elegante wijnen en geweldige service met een mooie glimlach!</p>
+<hr>
+<h5>GEORGE HEEFT HET ALLEMAAL</h5>
+<p>Van die lokale buurtplek waar iedereen je naam kent tot een licht, eclectisch restaurant met uitzicht op de Amstel: George heeft het allemaal.<br>
+
+We serveren iconische gerechten en tijdloze klassiekers met New York, Parijs, de Côte d'Azur en Hong Kong in gedachten, samen met lokale ingrediënten,vers gebrande koffie en kenmerkende cocktails. <br>
+Onze restaurants zijn de hele dag open, elke dag - dus kom bij ons voor een espresso in de vroege ochtend, een luie zondagse brunch of een intiem diner met vrienden in een ontspannen, elegante sfeer.<br>
+<hr>
+<h5>EEN FEEST VOOR JE ZINTUIGEN</h5>
+De meeste van onze restaurants zijn ontworpen door de Amsterdamse Architecture & Design studio Framework,<br> 
+die tekende voor onze op maat gemaakte interieurs en verfijnde maar intieme ambiance, met typische George-elementen zoals metrotegels, weelderige leren banken, koperen verlichting en marmeren bars. <br>
+Omdat elke locatie zijn eigen charme heeft, is uw bezoek aan George letterlijk een lust voor het oog en uw smaakpapillen.
+De meeste van onze locaties hebben prachtige, zonnige terrassen en zijn de hele dag open, elke dag voor ontbijt, lunch en diner.
+
+</div>
+
+
+</body>
+</html>
