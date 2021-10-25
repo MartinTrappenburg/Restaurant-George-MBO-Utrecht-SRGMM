de2f96bb4009b64b12424c259dccd2886d717884
Sherick16 <ajaxsdvbc3to@Gmail.com>
2021-10-25 20:53:49 +0200

    Updated landing, style, banner and navbar
    Updated landing, style, banner and navbar

--- banner.php
+++ banner.php
@@ -1,2 +1,2 @@
 <?php session_start(); session_gc(); ?>
-<img src="./img/logo.png" alt="RestaurantGeorge" class="img-fluid w-30 mx-auto d-block">
+<img src="./img/logo.png" alt="RestaurantGeorge" class="img-fluid w-30 mx-auto d-block" style="width: 15%;">
