5f9d3afa4865d4c05956cefa018511dbd24ffdf0
callofdeath <60213819+callofdeath@users.noreply.github.com>
2021-11-02 14:09:48 +0100

    Added recensies to navbar
    Added recensies to navbar
    Added recensie.php file

--- navbar.php
+++ navbar.php
@@ -60,6 +60,9 @@ $active = (isset($_GET["content"])) ? $_GET["content"] : "";
         <a class="nav-link" href="./index.php?content=carreerpage">CARREER</a>
       </li>
       <li class="nav-item <?php echo ($active == "smoothies") ? "active" : "" ?>">
+        <a class="nav-link" href="./index.php?content=recensies">RECENSIES</a>
+      </li>
+      <li class="nav-item <?php echo ($active == "smoothies") ? "active" : "" ?>">
         <a class="nav-link" href="./index.php?content=contactpage">CONTACT</a>
       </li>
       <li class="nav-item <?php echo ($active == "smoothies") ? "active" : "" ?>">
