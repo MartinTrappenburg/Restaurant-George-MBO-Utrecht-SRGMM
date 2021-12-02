29aa9205208761911ba71a071bd80c873216ff50
callofdeath <60213819+callofdeath@users.noreply.github.com>
2021-12-02 09:20:49 +0100

    Update roosters file
    Hoop informatie geupload in het bestand zelf, functionaliteit button uitklappen werkt nog niet

--- roosters.php
+++ roosters.php
@@ -1,14 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    Dit is een test
-    <!-- Required meta tags -->
-    <meta charset="UTF-8">
+<title>Roosters</title>
+</head>
+   <!-- Required meta tags -->
+   <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roosters</title>
-</head>
+
 <body>
-    
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Roosters
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Student</a></li>
+    <li><a class="dropdown-item" href="#">Begeleider</a></li>
+    <li><a class="dropdown-item" href="#">Eigenaar</a></li>
+  </ul>
+</div>
+<div class="container"> 
+<table class="table">
+<thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Maandag</th>
+      <th scope="col">Dinsdag</th>
+      <th scope="col">Woensdag</th>
+      <th scope="col">Donderdag</th>
+      <th scope="col">Vrijdag</th>
+</tr>
+</thead>
+  <tbody>
+    <tr>
+      <th scope="row">18:00</th>
+      <td>Kok</td>
+      <td>Barman</td>
+      <td>Kok</td>
+      <td>Kok</td>
+      <td>Barman</td>
+
+</tr>
+    <tr>
+      <th scope="row">19:00</th>
+      <td>Kok</td>
+      <td>Barman</td>
+      <td>Kok</td>
+      <td>Kok</td>
+      <td>Barman</td>
+
+</tr>
+    <tr>
+      <th scope="row">20:00</th>
+      <td>Ober</td>
+      <td>Algemeen</td>
+      <td>Ober</td>
+      <td>Ober</td>
+      <td>Algemeen</td>
+</tr>
+    <th scope="row">21:00</th>
+    <td>Ober</td>
+    <td>Algemeen</td>
+    <td>Ober</td>
+    <td>Ober</td>
+    <td>Algemeen</td>
+</tr>
+
+</tbody>
+</table>
 </body>
+</div>
+<h5>Locatie: Niels Bohrweg 121 in Maarssen</h5>
 </html>
