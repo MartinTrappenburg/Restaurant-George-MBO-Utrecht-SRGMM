f118c6310fb44e1a9287ac9144edd3f89f141f90
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-01-11 15:51:34 +0100

    Create dashboardcalender.php
    Added Dashboard Calender interface

---
+++ dashboardcalender.php
@@ -0,0 +1,35 @@
+<div id="jimbob" class="w-25 position-relative">
+    <h2>Welkom! Student.</h2>
+    <h3>Je bent nu bezig met het reserveren van een tijdslot.</h3> <br>
+  <div class="form-outline datepicker">
+    <label for="exampleDatepicker1" class="form-label">Selecteer uw gewenste datum.</label>
+    <input type="date" class="form-control" id="exampleDatepicker1">
+  </div>
+  <br>
+  <select class="form-select" aria-label="Default select example">
+    <option selected>Selecteer uw gewenste tijd.</option>
+    <option value="16:00">16:00</option>
+    <option value="16:30">16:30</option>
+    <option value="17:00">17:00</option>
+    <option value="16:00">17:30</option>
+    <option value="16:30">18:00</option>
+    <option value="17:00">18:30</option>
+    <option value="16:00">19:00</option>
+    <option value="16:30">19:30</option>
+    <option value="17:00">20:00</option>
+    <option value="16:00">20:30</option>
+    <option value="16:30">21:00</option>
+    <option value="17:00">21:30</option>
+    <option value="16:30">22:00</option>
+  </select>
+  <br>
+  <br>
+  <input type="checkbox" name="agree1" id="" required>Ik accepteer de privacy overeenkomst</input> <br>
+  <input type="checkbox" name="agree2" id="" required>Ik accepteer de algemene voorwaarden</input><br>
+  <input type="checkbox" name="agree3" id="">Ik wil de nieuwsbrief ontvangen</input>
+  <br>
+  <br>
+  <button type="button" class="btn btn-dark">Reserveer!</button>
+
+
+</div>
